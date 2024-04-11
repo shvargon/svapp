48da6b2658d570c0fea9351533db08357d12bd5a
Valery Shvargonov <animekazna@gmail.com>
2024-04-11 20:34:29 +0700

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex h-screen flex-col justify-between">
+	<header class="h-10 bg-red-500">Header</header>
+	<div class="container mx-auto mb-auto h-10 bg-green-500"><slot /></div>
+	<footer class="h-10 bg-blue-500">Footer</footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="myclass">Hello world!</h1>
+
+<style>
+	/* 
+		Убеждаемся что lint @apply проходит без проблем
+		https://github.com/tailwindlabs/tailwindcss/discussions/5258
+	*/
+	.myclass {
+		@apply text-gray-500;
+	}
+</style>
